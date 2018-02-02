42fba1649f477e2fda469a3700766636fae9739f
cadecahalan <cadecahalan@berkeley.edu>
2018-02-02 14:40:54 -0800

    Update 2v.js

--- 2v.js
+++ 2v.js
@@ -1,1 +1,20 @@
+background (255);
+strokeWeight (1);
 
+var count = 0
+
+line (width/2, count, width/2, count);
+
+while (count < (width/2)){
+  line ((width/2)-count, count, (width/2)+count, count);
+  count = count + 10;
+}
+
+count = 0
+
+while (count < width/2) {
+  line (count, (height/2)+count, width-count, (height/2)+count);
+  count = count + 10  
+}
+
+line (width/2, height, width/2, height);
