2f877f614b260fcb719efd3992e26e460c436904
cadecahalan <cadecahalan@berkeley.edu>
2018-02-02 14:46:13 -0800

    Create 2vi.js

---
+++ 2vi.js
@@ -0,0 +1,12 @@
+background (255);
+strokeWeight (1);
+
+var count = 5
+var countTall = 10
+
+while (count < (width)){
+  line (count, 0, count, countTall);
+  count = count + 5;
+  countTall = countTall + 10
+}
+
