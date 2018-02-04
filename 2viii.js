1c5f68d2f46ead7281b29ec337c47b8cc694bec5
cadecahalan <cadecahalan@berkeley.edu>
2018-02-03 16:25:54 -0800

    Create 2viii.js

---
+++ 2viii.js
@@ -0,0 +1,16 @@
+background (0);
+
+
+var count = 20
+var count2 = 20
+
+while (count < width){
+  ellipse(count, count2, 15, 15);
+  count = count + 20;
+if (count==width){
+  	count = 20
+  	count2 = count2 + 20
+}
+}
+
+//can't get it to stop at count2=height !! :(
