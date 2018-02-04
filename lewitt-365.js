276bef8e6b78a46cc0689f7bfaa878a22ca31c47
cadecahalan <cadecahalan@berkeley.edu>
2018-02-03 18:42:20 -0800

    Create lewitt-365.js

---
+++ lewitt-365.js
@@ -0,0 +1,15 @@
+function setup() { 
+  createCanvas(600, 325);
+	noStroke();
+} 
+
+function draw() { 
+  fill(220);
+	rect (0, 0, width/2, height/2);
+  fill(175);
+	rect (width/2, 0, width/2, height/2);
+  fill(130);
+	rect (0, height/2, width/2, height/2);
+  fill(90);
+	rect (width/2, height/2, width/2, height/2);
+}
